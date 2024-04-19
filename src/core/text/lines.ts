c64cbe466e2524577e52e4f1697ff9b968d1c666
ilya k <ilya.k423@gmail.com>
2024-04-19 19:20:06 +0300

    feat: added new features (fn linesIter) to core/text

--- src/core/text/lines.ts
+++ src/core/text/lines.ts
@@ -6,11 +6,39 @@ export function lines(s: string) {
 	return s.split(/\n+/);
 }
 
-export function linesIter(s: string) {
-	// todo
+export function linesIter(s: string){
+	// todo поправить регулярку
+	let rExpr = /(?<n>(|\n))(?<l>[a-zA-Zа-яА-Я-,.!?$# ]+)\k<n>/g;
+
+	let res = rExpr.exec(s);
+
+	return {
+		next() {
+			if (res == null) {
+				return {
+					value: null,
+					done: true,
+				}
+			}
+
+			// todo поправить работу с groups. Возможно, нужно сделать модуль по работе с регулярками.
+			const val = res.groups?.l;
+
+			res = rExpr.exec(s);
+
+			return {
+				value: val,
+				done: false,
+			}
+		},
+
+		[Symbol.iterator]() {
+			return this;
+		}
+	}
 }
 
-export function unLines(a:string[]) {
+export function unLines(a: string[]) {
 	if (a.length === 0) {
 		return ""
 	}
