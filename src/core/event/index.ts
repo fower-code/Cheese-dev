67624d5419516a9763aed3ed84f625d330db0a3e
ilya k <ilya.k423@gmail.com>
2024-04-26 23:04:19 +0300

    feat: added new features (method on) to core/event

--- src/core/event/index.ts
+++ src/core/event/index.ts
@@ -1,3 +1,45 @@
 export default class EventEmitter {
+	#handlers: Map<string, Function[]>;
 
+	get handlers() {
+		return this.#handlers;
+	}
+
+	constructor() {
+		this.#handlers = new Map();
+	}
+
+	private hasKey(event: string) {
+		return this.#handlers.has(event);
+	}
+
+	public clearHandlers(event: Event): void {
+
+	}
+
+	public once(event: string, cb: (v: unknown) => unknown) {
+
+	}
+
+	public on(event: string, cb: (v: unknown) => unknown) {
+		const
+			handlers = this.#handlers.get(event);
+
+		if (handlers) {
+			handlers.push(cb);
+			this.handlers.set(event, handlers);
+
+		} else {
+			this.handlers.set(event, [cb]);
+		}
+	}
+
+	public off(event: string, cb?: (v: unknown) => unknown) {
+		const
+			handlers = this.#handlers.get(event);
+	}
+
+	public emit(key: string, val: unknown) {
+
+	}
 };
