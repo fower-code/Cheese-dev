16ab38ab2d932047fa649c600583f4c9edb72edb
ilya k <ilya.k423@gmail.com>
2024-05-01 22:35:19 +0300

    feat: added new features (methods stream) to core/event

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,36 @@ function foo3(v: unknown) {
 }
 
 const ev1 = new EventEmitter();
-ev1.on("click", foo1);
-// ev1.on("click", foo2);
-ev1.once("click", foo2);
-ev1.on("click", foo3);
-// ev1.off("click", foo1);
-// ev1.off("close");
-// console.log(ev1.handlers);
-ev1.emit("click",1);
-ev1.emit("click",2);
+// ev1.on("click", foo1);
+// // ev1.on("click", foo2);
+// ev1.once("click", foo2);
+// ev1.on("click", foo3);
+// // ev1.off("click", foo1);
+// // ev1.off("close");
+// // console.log(ev1.handlers);
+// ev1.emit("click", 1);
+// ev1.emit("click", 2);
 
+// const el = document.getElementById("input");
+
+// if (el) {
+// 	console.log(el);
+//
+// 	(async () => {
+// 		for await (const ev of EventEmitter.stream(el, "input", foo1)) {
+// 			console.log(33);
+// 			// console.log(ev);
+// 		}
+// 	})();
+// }
+
+(async () => {
+	for await (const ev of EventEmitter.stream(ev1, "input", foo1)) {
+		console.log(33);
+		// console.log(ev);
+	}
+})();
+
+setInterval(() => {
+	ev1.emit("input", 1);
+}, 5000);
