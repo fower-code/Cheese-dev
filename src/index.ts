e8ce7efa74c6458bb1261e5dbc53584accab2fe1
ilya k <ilya.k423@gmail.com>
2024-05-03 21:10:55 +0300

    chore: core/iter/async

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,53 @@ import {asyncForeach} from "~core/iter/async";
 
 const r = range(1, 10);
 
-// export async function executeAsyncForeach<T>(asyncIter: AsyncIterable<T>): Promise<void> {
-// 	for await (const el of asyncIter) {
-//
-// 	}
-// }
+function createPromise() {
+	return new Promise((resolve, reject) => {
+		let timer = setTimeout(() => {
+			resolve(1);
+			clearTimeout(timer);
+
+		}, 3000);
+	})
+}
+
+function asyncIterPromise<T = Promise<unknown>>(iterable: Iterable<T>) {
+	const
+		iter = iterable[Symbol.iterator]();
+
+	return {
+		[Symbol.asyncIterator]() {
+			return this;
+		},
+
+		async next() {
+			const res = iter.next();
+
+			if (res.value) {
+				await res.value().then((v: unknown) => {
+					console.log(v);
+					return v;
+				});
+			}
+
+			return res;
+		}
+	};
+}
+
+const a1 = [
+	createPromise,
+	createPromise,
+];
 
 (async () => {
-	for await (const el of asyncForeach(r, 1, 3000)) {
-		console.log(2)
+	for await (const el of asyncIterPromise(a1)) {
+		console.log(el);
 	}
 })();
+
+// (async () => {
+// 	for await (const el of asyncForeach(r, 1, 3000)) {
+// 		console.log();
+// 	}
+// })();
