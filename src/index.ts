ca90c515070d9e2d82e223b8b3cfd6395709759b
ilya k <ilya.k423@gmail.com>
2024-05-05 23:52:45 +0300

    chore: core/iter/async

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,10 @@
 import range from "~core/range";
 import {asyncForeach} from "~core/iter/async";
 
-const r = range(1, 10);
-
-function createPromise(n: number) {
-	return new Promise((resolve, reject) => {
-		let timer = setTimeout(() => {
-			resolve(n);
-			clearTimeout(timer);
-
-		}, 3000);
-	})
-}
-
-// function asyncIterPromise<T = Promise<unknown>>(iterable: Iterable<T>) {
-// 	const
-// 		iter = iterable[Symbol.iterator]();
-//
-// 	return {
-// 		[Symbol.asyncIterator]() {
-// 			return this;
-// 		},
-//
-// 		async next() {
-// 			const res = iter.next();
-//
-// 			if (res.value) {
-// 				await res.value().then((v: unknown) => {
-// 					console.log(v);
-// 					return v;
-// 				});
-// 			}
-//
-// 			return res;
-// 		}
-// 	};
-// }
-
-const a1 = [
-	1,
-	2
-];
-
-function asyncIterConvertPromise() {
-
-}
+const r = range(1, 100);
 
 (async () => {
-	for await (const el of a1) {
-		// console.log(el);
-		await createPromise(el).then((v) => {
-			console.log(v)
-		});
+	for await (const el of asyncForeach(r, 1, 3000)) {
+		console.log(el);
 	}
 })();
-
-// (async () => {
-// 	for await (const el of asyncForeach(r, 1, 3000)) {
-// 		console.log();
-// 	}
-// })();
