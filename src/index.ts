dc481e1e6101780a8aa1d00d3914a719b4356553
ilya <iosdsbb@gmail.com>
2024-06-20 02:33:39 +0300

    feat: removed core/iter/green-thread

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import range from "~core/range";
-import Iter from "~core/iter";
-import {createIterThread, executorIterThread} from "~core/iter/green-thread";
-
-const r = range(1, 4000);
-
-const threads = createIterThread(r, 4);
-console.log(threads);
-
-executorIterThread(threads, (chunk) => {
-	console.log(chunk);
-});
 
