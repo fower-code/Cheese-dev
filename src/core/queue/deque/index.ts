fba311158214ff8e30a8afeba094aefe261fd3ce
ilya <iosdsbb@gmail.com>
2024-06-20 14:22:11 +0300

    feat: added new features to core/queue/deque

--- src/core/queue/deque/index.ts
+++ src/core/queue/deque/index.ts
@@ -35,4 +35,50 @@ export default class Deque<T> {
 	constructor(iterable?: Iterable<T>) {
 		this.innerStruct = new DoubleLinkedList<T>(iterable);
 	}
+
+
+	/**
+	 * @description Добавляет элемент в начало очереди.
+	 * @param item Новый элемент очереди
+	 */
+	public insertFirst(item: T) {
+		this.innerStruct.insertFirst(item);
+	}
+
+	/**
+	 * @description Извлекает элемент из начала очереди.
+	 */
+	public removeFirst() {
+		return this.innerStruct.removeFirst();
+	}
+
+	/**
+	 * @description Добавляет элемент в конец очереди.
+	 * @param item Новый элемент очереди
+	 */
+	public insertLast(item: T) {
+		this.innerStruct.insertLast(item);
+	}
+
+	/**
+	 * @description Извлекает элемент из конца очереди.
+	 */
+	public removeLast() {
+		return this.innerStruct.removeLast();
+	}
+
+	/**
+	 * @description Возвращает true, если очередь пуста, и false в обратном случае
+	 * @return boolean
+	 */
+	public isEmpty(): boolean {
+		return this.innerStruct.isEmpty();
+	}
+
+	/**
+	 * @description Очищает очередь
+	 */
+	public clear(): void {
+		this.innerStruct.clear();
+	}
 };
