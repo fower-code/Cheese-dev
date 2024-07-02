dd324229e99fc657f04935afac92cf532f06544b
ilya <iosdsbb@gmail.com>
2024-07-02 19:19:13 +0300

    feat: added threadOnline

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import {isOnline} from "~core/net";
+// import {isOnline} from "~core/net";
+//
+import {iterInterval} from "~core/iter";
 
 const successCallback = () => {
 	console.log("successCallback");
@@ -7,12 +9,27 @@ const successCallback = () => {
 const errorCallback = () => {
 	console.log("errorCallback");
 };
+//
+// isOnline(errorCallback, successCallback)
+// 	.then((data) => {
+// 		console.log("success");
+//
+// 	})
+// 	.catch((err) => {
+// 		console.log("err: ", err);
+// 	});
 
-isOnline(errorCallback, successCallback)
-	.then((data) => {
-		console.log("success");
+import {threadOnline} from "~core/net/engines/browser";
 
-	})
-	.catch((err) => {
-		console.log("err: ", err);
-	});
+const threadIter = threadOnline(errorCallback, successCallback);
+
+(async () => {
+	if (!threadIter) {
+		return;
+	}
+
+	for await (const status of iterInterval(threadIter, 2000)) {
+		// console.log("Итерация завершилась");
+		console.log(`user in online ${status}`);
+	}
+})();
