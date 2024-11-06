5b571b6d3fab000a977dd4d66ebd6c0d448f5f3f
Ilya <iosdsbb@gmail.com>
2024-11-06 23:29:24 +0300

    test: added moduleNameMapper to jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -4,10 +4,12 @@ const config: Config = {
 	rootDir: './',
 	projects: ["<rootDir>"],
 	testMatch: [
-		// '<rootDir>/dist/server/**/*[sS]pec.js',
 		"<rootDir>/src/**/*[sS]pec.ts",
 		"<rootDir>/src/**/?(*.)+(spec).ts"
 	],
+	moduleNameMapper: {
+		"~core/(.*)": "<rootDir>/src/core/$1",
+	},
 
 	preset: "ts-jest",
 	testEnvironment: "node",
