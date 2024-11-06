40c71fc4c6405430ea209153d1aeb17934e96cc4
Ilya <iosdsbb@gmail.com>
2024-11-06 23:29:41 +0300

    test: added iter/spec.ts

---
+++ src/core/iter/spec.ts
@@ -0,0 +1,13 @@
+import {map} from "~core/iter/index";
+
+describe("core/iter", () => {
+	test("map", () => {
+		const iter = map([1, 2, 3, 4], ((el) => el * 2));
+		expect([...iter]).toEqual([2, 4, 6, 8]);
+	});
+
+	test("map", () => {
+		const iter = map([], ((el) => el * 2));
+		expect([...iter]).toEqual([]);
+	});
+});
