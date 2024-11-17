899dbef81a0bb83cf5ffc6b08c1d9dd261488798
Ilya <iosdsbb@gmail.com>
2024-11-17 19:43:56 +0300

    infra: updated .husky/pre-push

--- src/core/fn-tools/index.ts
+++ src/core/fn-tools/index.ts
@@ -3,7 +3,7 @@
  * @param fn
  * @param time
  */
-	export function debounce(fn: Function, time: number) {
+export function debounce(fn: Function, time: number) {
 	let timeout: CanNull<ReturnType<typeof setTimeout>>;
 
 	return function (this: unknown, ...args: unknown[]) {
