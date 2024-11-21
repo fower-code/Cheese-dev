149b7d63be35438de59c9592d739d6e99b50f376
Ilya <iosdsbb@gmail.com>
2024-11-21 23:23:29 +0300

    chore: testing Range

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,14 @@ import {Range} from "~core/range";
 
 esbuildApiInit();
 
-// // 1, 2, 3, 4, 5
-// for (const i of range(1)) {
-// 	console.log(i);
-//
-// 	if (i >= 5) {
-// 		break;
-// 	}
-// }
-//
-// // 1, 2, 3, 4, 5
-// for (const i of range(1, 5)) {
-// 	console.log(i);
-// }
-//
-// [1, 2, 3, 4, 5]
-// console.log([...range(1, 5)]);
+const r = new Range("a", "c");
+console.log([...r]); // ['a', 'b', 'c']
 
-// const r = new Range(1, "c");
-// console.log([...r]);
+const r2 = new Range(1, 3);
+console.log([...r2]); // [1, 2, 3]
 
 for (const n of new Range(1,3)) {
 	console.log(n); // Outputs: 1, 2, 3
 }
+
+const r4 = new Range(1, "c"); // Uncaught TypeError: Start and end must be of the same type.
