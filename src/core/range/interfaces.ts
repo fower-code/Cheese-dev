6d42cae65d1cd9b1e1834221cda18cd631c0a811
Ilya <iosdsbb@gmail.com>
2024-11-21 23:13:14 +0300

    docs: added desc for RangeType, RangeVal

--- src/core/range/interfaces.ts
+++ src/core/range/interfaces.ts
@@ -1,3 +1,9 @@
+/**
+ * Type representing the acceptable values for the range.
+ */
 export type RangeVal = "number" | "string";
 
+/**
+ * Type representing the value type in the range.
+ */
 export type RangeType = number | string;
