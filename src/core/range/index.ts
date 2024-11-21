d9e46e2e770fef2bcf2dcd986b2937bc217d25f5
Ilya <iosdsbb@gmail.com>
2024-11-21 22:17:19 +0300

    feat: added class Range

--- src/core/range/index.ts
+++ src/core/range/index.ts
@@ -1,8 +1,12 @@
+import { RangeType, RangeVal } from "~core/range/interfaces";
+
 export function range(
-	start: number,
-	end?: number,
+	start: RangeVal,
+	end?: RangeVal,
 ): IterableIterator<number> {
-	let i = start;
+	let i = typeof start === "number" ? start : start.charCodeAt(0);
+	console.log(i);
+	// let i = start;
 
 	if (end) {
 		return {
@@ -38,4 +42,68 @@ export function range(
 			return this;
 		},
 	};
+}
+
+export class Range {
+	protected start: number;
+	protected end: number;
+	protected type: RangeVal;
+
+	constructor(start: RangeType, end?: RangeType) {
+		if (end && typeof start !== typeof end) {
+			throw new TypeError("");
+		}
+
+		this.type = typeof start === "number" ? "number" : "string";
+		this.start = typeof start === "number" ? start : start.charCodeAt(0);
+		this.end =
+			(typeof end === "number" ? end : end?.charCodeAt(0)) ?? Infinity;
+	}
+
+	[Symbol.iterator]() {
+		let i = this.start;
+		const that = this;
+
+		if (this.type === "number") {
+			return {
+				next() {
+					if (i <= that.end) {
+						return {
+							value: i++,
+							done: false,
+						};
+					}
+
+					return {
+						value: null,
+						done: true,
+					};
+				},
+
+				[Symbol.iterator]() {
+					return this;
+				},
+			};
+		} else {
+			return {
+				next() {
+					if (i <= that.end) {
+						return {
+							value: String.fromCharCode(i++),
+							done: false,
+						};
+					}
+
+					return {
+						value: null,
+						done: true,
+					};
+				},
+
+				[Symbol.iterator]() {
+					return this;
+				},
+			};
+		}
+	}
 }
