f4dc67d3287c59e394fe45bb7df6d5761bb70721
Ilya <iosdsbb@gmail.com>
2024-12-09 23:22:23 +0300

    feat(kv-storage): added SerializableVal, SerializablePrimitiveVal

--- src/core/kv-storage/interfaces.ts
+++ src/core/kv-storage/interfaces.ts
@@ -1,5 +1,17 @@
-import {KVStorageEngine} from "~core/kv-storage/engines/interfaces";
+import { KVStorageEngine } from "~core/kv-storage/engines/interfaces";
 
 export interface KVStorageOptions {
 	engine?: KVStorageEngine;
 }
+
+export type SerializableVal =
+	SerializablePrimitiveVal |
+	SerializablePrimitiveVal[] |
+	Record<string, SerializablePrimitiveVal> |
+	{ toJSON(): SerializableVal };
+
+export type SerializablePrimitiveVal =
+	string |
+	number |
+	boolean |
+	null;
