09f68057995a54eced50aa085bba3dd2c47f7496
Ilya <iosdsbb@gmail.com>
2024-12-09 23:39:58 +0300

    feat(kv-storage): added engine to KVStorage

--- src/core/kv-storage/index.ts
+++ src/core/kv-storage/index.ts
@@ -1,3 +1,10 @@
-export class KVStorage {
+import { KVStorageEngine, KVStorageOptions } from "~core/kv-storage/interfaces";
+import DefaultEngine from "~core/kv-storage/engines";
 
+export default class KVStorage {
+	protected engine: KVStorageEngine;
+
+	constructor(opts: KVStorageOptions) {
+		this.engine = opts.engine ?? new DefaultEngine();
+	}
 }
